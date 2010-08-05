dfd4ead68055033c06158fa952442104bc389a7d
meklort <meklort@b5af796a-00a8-11df-919b-ffff7a100b7d>
2010-08-05 01:32:58 +0000

    Removed autogenerated files. Removed HelloWorld.h

--- i386/modules/HelloWorld/HelloWorldInit.c
+++
@@ -1,7 +0,0 @@
-extern int load_dependency(const char*);
-
-int start() {
-	int returnValue = 1;
-	if(returnValue) returnValue &= load_dependency("Symbols");
-	return returnValue;
-}
--- i386/modules/Symbols/SymbolsInit.c
+++
@@ -1,6 +0,0 @@
-extern int load_dependency(const char*);
-
-int start() {
-	int returnValue = 1;
-	return returnValue;
-}
