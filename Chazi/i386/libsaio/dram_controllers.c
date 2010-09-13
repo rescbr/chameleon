7d82ac5f8a4a71eea5d6ffcbb077f9270a36f042
azimutz <azimutz@b5af796a-00a8-11df-919b-ffff7a100b7d>
2010-09-13 06:46:05 +0000

    Trunk it, rev 517.

--- Chazi/i386/libsaio/dram_controllers.c
+++ Chazi/i386/libsaio/dram_controllers.c
@@ -471,10 +471,10 @@ static void get_timings_nhm(pci_dt_t *dram_dev)
 	Platform.RAM.TRC = (mc_channel_bank_timing >> 9) & 0xF; 
 	
 	// RAS Precharge (tRP)
-	Platform.RAM.CAS = (mc_channel_bank_timing >> 4) & 0x1F; 
+	Platform.RAM.TRP = mc_channel_bank_timing & 0xF;
 	
 	// RAS Active to precharge (tRAS)
-	Platform.RAM.RAS = mc_channel_bank_timing & 0xF;
+	Platform.RAM.RAS = (mc_channel_bank_timing >> 4) & 0x1F;
 	
 	// Single , Dual or Triple Channels
 	if (mc_control == 1 || mc_control == 2 || mc_control == 4 )
