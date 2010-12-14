60f0645252fc9d9fd43c68b7e9639c60a22cecbd
valv <valv@b5af796a-00a8-11df-919b-ffff7a100b7d>
2010-12-14 12:39:58 +0000

    moving to root

--- branch/i386/libsaio/freebsd.h
+++
@@ -1,2 +0,0 @@
-extern bool FreeBSDProbe (const void *buf);
-extern void FreeBSDGetDescription(CICell ih, char *str, long strMaxLen);
--- branch/i386/libsaio/openbsd.h
+++
@@ -1,2 +0,0 @@
-extern bool OpenBSDProbe (const void *buf);
-extern void OpenBSDGetDescription(CICell ih, char *str, long strMaxLen);
