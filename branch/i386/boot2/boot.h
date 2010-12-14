1e5eee2e67cf838960c3c6443925fc89f21bcbcb
valv <valv@b5af796a-00a8-11df-919b-ffff7a100b7d>
2010-12-14 12:31:45 +0000

    latest update of my branch

---
+++ branch/i386/boot2/boot.h
@@ -0,0 +1,297 @@
+/*
+ * Copyright (c) 1999-2003 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * Portions Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights
+ * Reserved.  This file contains Original Code and/or Modifications of
+ * Original Code as defined in and that are subject to the Apple Public
+ * Source License Version 2.0 (the "License").  You may not use this file
+ * except in compliance with the License.  Please obtain a copy of the
+ * License at http://www.apple.com/publicsource and read it before using
+ * this file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON- INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ */
+/*
+ * Copyright 1994 NeXT Computer, Inc.
+ * All rights reserved.
+ */
+
+#ifndef __BOOT2_BOOT_H
+#define __BOOT2_BOOT_H
+
+#include "libsaio.h"
+#include "autoresolution.h"
+ 
+#if DEBUG
+#ifndef AUTORES_DEBUG
+#define AUTORES_DEBUG
+#endif
+#endif
+/*
+ * Keys used in system Boot.plist
+ */
+#define kGraphicsModeKey	"Graphics Mode"
+#define kTextModeKey		"Text Mode"
+#define kQuietBootKey		"Quiet Boot"
+#define kKernelFlagsKey		"Kernel Flags"
+#define kMKextCacheKey		"MKext Cache"
+#define kKernelNameKey		"Kernel"
+#define kKernelCacheKey		"Kernel Cache"
+#define kBootDeviceKey		"Boot Device"
+#define kTimeoutKey		"Timeout"
+#define kRootDeviceKey		"rd"
+#define kBootUUIDKey		"boot-uuid"
+#define kHelperRootUUIDKey	"Root UUID"
+#define kPlatformKey		"platform"
+#define kACPIKey		"acpi"
+#define kCDROMPromptKey		"CD-ROM Prompt"
+#define kCDROMOptionKey		"CD-ROM Option Key"
+#define kRescanPromptKey	"Rescan Prompt"
+#define kRescanKey		"Rescan"
+#define kScanSingleDriveKey	"Scan Single Drive"
+#define kInsantMenuKey		"Instant Menu"
+#define kDefaultKernel		"mach_kernel"
+#define kGUIKey			"GUI"
+#define kBootBannerKey		"Boot Banner"
+#define kWaitForKeypressKey	"Wait"
+
+/* AsereBLN: added these keys */
+#define kUseAtiROM		"UseAtiROM"		/* ati.c */
+#define kWake			"Wake"			/* boot.c */
+#define kForceWake		"ForceWake"		/* boot.c */
+#define kWakeImage		"WakeImage"		/* boot.c */
+#define kProductVersion		"ProductVersion"	/* boot.c */
+#define karch			"arch"			/* boot.c */
+#define kDSDT			"DSDT"			/* acpi_patcher.c */
+#define kDropSSDT		"DropSSDT"		/* acpi_patcher.c */
+#define kDeviceProperties	"device-properties"	/* device_inject.c */
+#define kHidePartition		"Hide Partition"	/* disk.c */
+#define kRenamePartition	"Rename Partition"	/* disk.c */
+#define kSMBIOS			"SMBIOS"		/* fake_efi.c */
+#define kSystemID		"SystemId"		/* fake_efi.c */
+#define kSystemType		"SystemType"		/* fake_efi.c */
+#define kUseNvidiaROM		"UseNvidiaROM"		/* nvidia.c */
+#define kVBIOS			"VBIOS"			/* nvidia.c */
+#define kPCIRootUID		"PCIRootUID"		/* pci_root.c */
+#define kEthernetBuiltIn	"EthernetBuiltIn"	/* pci_setup.c */
+#define kGraphicsEnabler	"GraphicsEnabler"	/* pci_setup.c */
+#define kForceHPET		"ForceHPET"		/* pci_setup.c */
+#define kUseMemDetect		"UseMemDetect"		/* platform.c */
+#define kSMBIOSdefaults		"SMBIOSdefaults"	/* smbios_patcher.c */
+#define kUSBBusFix		"USBBusFix"		/* usb.c */
+#define kEHCIacquire		"EHCIacquire"		/* usb.c */
+#define kUHCIreset		"UHCIreset"		/* usb.c */
+#define kEHCIhard		"EHCIhard"		/* usb.c */
+#define kDefaultPartition	"Default Partition"	/* sys.c */
+
+/* Duvel300: added this */
+#define kRestartFix		"RestartFix"		/* acpi_patcher.c */
+
+/* Signal64: added this key */
+#define kUSBLegacyOff		"USBLegacyOff"		/* usb.c */
+
+/* Lebidou: added this key */
+#define kAutoResolutionKey	"AutoResolution"
+
+/* Meklort: added this key */
+#define kMD0Image		"md0"			/* ramdisk.h */
+
+/* Andyvand: added these keys */
+#define kSSDT			"SSDT"			/* acpi_patcher.c */
+#define kHPET			"HPET"			/* acpi_patcher.c */
+#define kSBST			"SBST"			/* acpi_patcher.c */
+#define kECDT			"ECDT"			/* acpi_patcher.c */
+#define kASFT			"ASFT"			/* acpi_patcher.c */
+#define kDMAR			"DMAR"			/* acpi_patcher.c */
+#define kFADT			"FADT"			/* acpi_patcher.c */
+#define kAPIC			"APIC"			/* acpi_patcher.c */
+#define kMCFG			"MCFG"			/* acpi_patcher.c */
+#define kOEMDSDT		"oemDSDT"		/* acpi_patcher.c */
+#define kOEMSSDT		"oemSSDT"		/* acpi_patcher.c */
+#define kOEMHPET		"oemHPET"		/* acpi_patcher.c */
+#define kOEMSBST		"oemSBST"		/* acpi_patcher.c */
+#define kOEMECDT		"oemECDT"		/* acpi_patcher.c */
+#define kOEMASFT		"oemASFT"		/* acpi_patcher.c */
+#define kOEMDMAR		"oemDMAR"		/* acpi_patcher.c */
+#define kOEMFADT		"oemFADT"		/* acpi_patcher.c */
+#define kOEMAPIC		"oemAPIC"		/* acpi_patcher.c */
+#define kOEMMCFG		"oemMCFG"		/* acpi_patcher.c */
+#define kDropHPET		"DropHPET"		/* acpi_patcher.c */
+#define kDropSLIC		"DropSLIC"		/* acpi_patcher.c */
+#define kDropSBST		"DropSBST"		/* acpi_patcher.c */
+#define kDropECDT		"DropECDT"		/* acpi_patcher.c */
+#define kDropASFT		"DropASFT"		/* acpi_patcher.c */
+#define kDropDMAR		"DropDMAR"		/* acpi_patcher.c */
+#define kUpdateACPI		"UpdateACPI"		/* acpi_patcher.c */
+
+/* Mojodojo: added these keys */
+#define kGeneratePStates	"GeneratePStates"	/* acpi_patcher.c */
+#define kGenerateCStates	"GenerateCStates"	/* acpi_patcher.c */
+#define kEnableC2State		"EnableC2State"		/* acpi_patcher.c */
+#define kEnableC3State		"EnableC3State"		/* acpi_patcher.c */
+#define kEnableC4State		"EnableC4State"		/* acpi_patcher.c */
+
+/* valv: added these keys */
+#define kFixFSB				"FixFSB"			/* cpu.c */
+#define kbusratio			"busratio"			/* cpu.c */
+#define kForceFSB			"ForceFSB"			/* cpu.c */
+#define kC2EEnable			"C2E"				/* cpu.c */
+#define kC4EEnable			"C4E"				/* cpu.c */
+#define kHardC4EEnable		"HC4E"				/* cpu.c */
+#define kdcfg0				"display_0"			/* nvidia.c */
+#define kdcfg1				"display_1"			/* nvidia.c */
+#define kpstates			"PStates"			/* acpi_patcher.c */
+#define kEnableC6State		"EnableC6State"		/* acpi_patcher.c */
+#define kmaxVoltage			"maxVolt"			/* acpi_patcher.c */
+#define kminVoltage			"minVolt"			/* acpi_patcher.c */
+#define kmoreVoltage		"moreVolt"			/* acpi_patcher.c */
+#define klessVoltage		"lessVolt"			/* acpi_patcher.c */
+#define kTjmax				"Tjmax"				/* acpi_patcher.c */
+#define kpstart				"PStart"			/* acpi_patcher.c */
+
+/*
+ * Flags to the booter or kernel
+ */
+#define kVerboseModeFlag	"-v"
+#define kSafeModeFlag		"-x"
+#define kIgnoreCachesFlag	"-f"
+#define kIgnoreBootFileFlag	"-F"
+#define kSingleUserModeFlag	"-s"
+#define k32BitModeFlag		"-x32"
+#define k64BitModeFlag		"-x64"
+
+/*
+ * Booter behavior control
+ */
+#define kBootTimeout         -1
+#define kCDBootTimeout       8
+
+/*
+ * A global set by boot() to record the device that the booter
+ * was loaded from.
+ */
+extern int  gBIOSDev;
+extern long gBootMode;
+extern bool sysConfigValid;
+extern char bootBanner[];
+extern char bootPrompt[];
+extern bool gOverrideKernel;
+extern char *gPlatformName;
+extern char gMKextName[];
+extern char gRootDevice[];
+extern bool gEnableCDROMRescan;
+extern bool gScanSingleDrive;
+extern bool useGUI;
+
+/*
+ * Boot Modes
+ */
+enum {
+    kBootModeNormal = 0,
+    kBootModeSafe   = 1,
+    kBootModeSecure = 2,
+    kBootModeQuiet  = 4
+};
+
+extern void initialize_runtime();
+extern void common_boot(int biosdev);
+
+/*
+ * usb.c
+ */
+extern int usb_loop();
+
+/*
+ * graphics.c
+ */
+extern void printVBEModeInfo();
+extern void setVideoMode(int mode, int drawgraphics);
+extern int  getVideoMode();
+extern void spinActivityIndicator();
+extern void clearActivityIndicator();
+extern void drawColorRectangle( unsigned short x,
+                         unsigned short y,
+                         unsigned short width,
+                         unsigned short height,
+                         unsigned char  colorIndex );
+extern void drawDataRectangle( unsigned short  x,
+                        unsigned short  y,
+                        unsigned short  width,
+                        unsigned short  height,
+                               unsigned char * data );
+extern int
+convertImage( unsigned short width,
+              unsigned short height,
+              const unsigned char *imageData,
+              unsigned char **newImageData );
+extern char * decodeRLE( const void * rleData, int rleBlocks, int outBytes );
+extern void drawBootGraphics(void);
+extern void drawPreview(void *src, uint8_t * saveunder);
+extern int getVideoMode(void);
+extern void loadImageScale (void *input, int iw, int ih, int ip, void *output, int ow, int oh, int op, int or);
+
+/*
+ * drivers.c
+ */
+extern long LoadDrivers(char * dirSpec);
+extern long DecodeKernel(void *binary, entry_t *rentry, char **raddr, int *rsize);
+
+typedef long (*FileLoadDrivers_t)(char *dirSpec, long plugin);
+/*!
+    Hookable function pointer called during the driver loading phase that
+    allows other code to cause additional drivers to be loaded.
+ */
+extern long (*LoadExtraDrivers_p)(FileLoadDrivers_t FileLoadDrivers_p);
+
+/*
+ * options.c
+ */
+extern int getBootOptions(bool firstRun);
+extern int processBootOptions();
+extern int selectAlternateBootDevice(int bootdevice);
+extern bool promptForRescanOption(void);
+
+void showHelp();
+void showTextFile();
+char *getMemoryInfoString();
+
+typedef struct {
+    char   name[80];
+    void * param;
+} MenuItem;
+
+/*
+ * lzss.c
+ */
+extern int decompress_lzss(u_int8_t *dst, u_int8_t *src, u_int32_t srclen);
+
+struct compressed_kernel_header {
+  u_int32_t signature;
+  u_int32_t compress_type;
+  u_int32_t adler32;
+  u_int32_t uncompressed_size;
+  u_int32_t compressed_size;
+  u_int32_t reserved[11];
+  char      platform_name[64];
+  char      root_path[256];
+  u_int8_t  data[0];
+};
+typedef struct compressed_kernel_header compressed_kernel_header;
+
+/* resume.c */
+void HibernateBoot(char *boot_device);
+
+/* bmdecompress.c */
+void * DecompressData(void *srcbase, int *dw, int *dh, int *bytesPerPixel);
+
+#endif /* !__BOOT2_BOOT_H */
---
+++ branch/i386/boot2/graphics.h
@@ -0,0 +1,58 @@
+/*
+ *  graphics.h
+ *  
+ *
+ *  Created by fassl on 22.12.08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "boot.h"
+#include "bootstruct.h"
+#include "graphic_utils.h"
+#include "vbe.h"
+
+
+#ifndef __BOOT_GRAPHICS_H
+#define __BOOT_GRAPHICS_H
+
+#define DEFAULT_SCREEN_WIDTH 1024
+#define DEFAULT_SCREEN_HEIGHT 768
+
+int loadPngImage(const char *filename, uint16_t *width, uint16_t *height, uint8_t **imageData);
+
+unsigned long lookUpCLUTIndex( unsigned char index, unsigned char depth );
+
+void drawColorRectangle( unsigned short x, unsigned short y, unsigned short width, unsigned short height, unsigned char  colorIndex );
+void drawDataRectangle( unsigned short x, unsigned short y, unsigned short width, unsigned short height, unsigned char * data );
+int convertImage( unsigned short width, unsigned short height, const unsigned char *imageData, unsigned char **newImageData );
+
+int initGraphicsMode ();
+
+void drawCheckerBoard();
+
+void blendImage(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t *data);
+
+void drawCheckerBoard();
+void blendImage(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t *data);
+
+int loadEmbeddedPngImage(uint8_t *pngData, int pngSize, uint16_t *width, uint16_t *height, uint8_t **imageData);
+
+int getNumberArrayFromProperty( const char *  propKey,
+						   unsigned long numbers[],
+						   unsigned long maxArrayCount );
+
+
+char *getVBEInfoString();
+char *getVBEModeInfoString();
+unsigned short getVESAModeWithProperties( unsigned short     width,
+										 unsigned short     height,
+										 unsigned char      bitsPerPixel,
+										 unsigned short     attributesSet,
+										 unsigned short     attributesClear,
+										 VBEModeInfoBlock * outModeInfo,
+										 unsigned short *   vesaVersion );
+
+void getGraphicModeParams(unsigned long params[]);
+
+#endif /* !__BOOT_GRAPHICS_H */
