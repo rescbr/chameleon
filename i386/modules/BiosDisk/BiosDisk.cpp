ccd7cac2e8b675c468653676b9c7e49e70c9a8b3
meklort <meklort@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-06-25 17:14:32 +0000

    Added c++ entrypoint files for module disk + fs modules

---
+++ i386/modules/BiosDisk/BiosDisk.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011 Evan Lojewski. All rights reserved.
+ *
+ */
+#include <cstdlib>
+#include <iostream>
+#include <modules>
+
+extern "C"
+{
+    void BiosDisk_start();
+}
+
+void BiosDisk_start()
+{
+}
---
+++ i386/modules/FileSystem/FileSystem.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011 Evan Lojewski. All rights reserved.
+ *
+ */
+#include <cstdlib>
+#include <iostream>
+#include <modules>
+
+extern "C"
+{
+    void FileSystem_start();
+}
+
+void FileSystem_start()
+{
+}
