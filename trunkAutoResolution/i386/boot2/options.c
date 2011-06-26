6b588d44cf2c569a7e93692015ee8bc19ec94feb
azimutz <azimutz@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-06-26 00:26:30 +0000

    Runaway "min"; fixes build.

--- trunkAutoResolution/i386/boot2/options.c
+++ trunkAutoResolution/i386/boot2/options.c
@@ -1047,7 +1047,7 @@ int getBootOptions(bool firstRun)
 						if (gDeviceCount)
 						{
 							showMenu( menuItems, gDeviceCount, selectIndex, kMenuTopRow + 2, kMenuMaxItems );
-							nextRow += min( gDeviceCount, kMenuMaxItems ) + 3;
+							nextRow += MIN( gDeviceCount, kMenuMaxItems ) + 3;
 						}
 
 						// Show the boot prompt.
