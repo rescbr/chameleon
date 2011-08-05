3efd8fdebce60f324a500d183dd72c20eb539adf
meklort <meklort@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-08-05 19:35:56 +0000

    Addmissing symbol in modules_support.s.

--- src/modules/ModuleSystem/i386/modules_support.s
+++ src/modules/ModuleSystem/i386/modules_support.s
@@ -1,6 +1,9 @@
 #ifdef CONFIG_MODULESYSTEM_MODULE
 #include <architecture/i386/asm_help.h>
-	
+
+LABEL(dyld_stub_binder)
+	jmp		_dyld_stub_binder
+
 LABEL(dyld_void_start)
 LABEL(_ModuleSystem_start)
     ret
