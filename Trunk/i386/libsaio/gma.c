aa238a64998fb2849ce2b35f1132c463099d5010
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2011-10-09 17:47:03 +0000

    Work in Progress 2 (Restructure my branch... Trunk)

---
+++ Trunk/i386/libsaio/gma.c
@@ -0,0 +1,300 @@
+/*
+	Original patch by Nawcom
+	http://forum.voodooprojects.org/index.php/topic,1029.0.html
+*/
+
+#include "libsa.h"
+#include "saio_internal.h"
+#include "bootstruct.h"
+#include "pci.h"
+#include "platform.h"
+#include "device_inject.h"
+#include "gma.h"
+
+#ifndef DEBUG_GMA
+#define DEBUG_GMA 0
+#endif
+
+#if DEBUG_GMA
+#define DBG(x...)	printf(x)
+#else
+#define DBG(x...)
+#endif
+
+
+uint8_t GMAX3100_vals[22][4] = {
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x08 },
+	{ 0x64,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x08 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x20,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x20,0x03,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x08,0x52,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+	{ 0x3B,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 }
+};
+
+uint8_t HD2000_vals[16][4] = {
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x14,0x00,0x00,0x00 },
+	{ 0xfa,0x00,0x00,0x00 },
+	{ 0x2c,0x01,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x14,0x00,0x00,0x00 },
+	{ 0xf4,0x01,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+};
+
+uint8_t HD3000_vals[16][4] = {
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x14,0x00,0x00,0x00 },
+	{ 0xfa,0x00,0x00,0x00 },
+	{ 0x2c,0x01,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x14,0x00,0x00,0x00 },
+	{ 0xf4,0x01,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x00,0x00,0x00,0x00 },
+	{ 0x01,0x00,0x00,0x00 },
+};
+
+uint8_t HD2000_tbl_info[18] = {
+	0x30,0x44,0x02,0x02,0x02,0x02,0x00,0x00,0x00,
+	0x00,0x01,0x02,0x02,0x02,0x00,0x01,0x02,0x02
+};
+uint8_t HD2000_os_info[20] = {
+	0x30,0x49,0x01,0x11,0x11,0x11,0x08,0x00,0x00,0x01,
+	0xf0,0x1f,0x01,0x00,0x00,0x00,0x10,0x07,0x00,0x00
+};
+
+// The following values came from a Sandy Bridge MacBook Air
+uint8_t HD3000_tbl_info[18] = {
+	0x30,0x44,0x02,0x02,0x02,0x02,0x00,0x00,0x00,
+	0x00,0x02,0x02,0x02,0x02,0x01,0x01,0x01,0x01
+};
+
+// The following values came from a Sandy Bridge MacBook Air
+uint8_t HD3000_os_info[20] = {
+	0x30,0x49,0x01,0x12,0x12,0x12,0x08,0x00,0x00,0x01,
+	0xf0,0x1f,0x01,0x00,0x00,0x00,0x10,0x07,0x00,0x00
+};
+
+
+uint8_t reg_TRUE[]	= { 0x01, 0x00, 0x00, 0x00 };
+uint8_t reg_FALSE[] = { 0x00, 0x00, 0x00, 0x00 };
+
+static struct gma_gpu_t KnownGPUS[] = {
+	{ 0x00000000, "Unknown"			},
+	{ 0x808627A2, "Mobile GMA950"	},
+	{ 0x808627AE, "Mobile GMA950"	},
+	{ 0x808627A6, "Mobile GMA950"	},
+	{ 0x8086A011, "Mobile GMA3150"	},
+	{ 0x8086A012, "Mobile GMA3150"	},
+	{ 0x80862772, "Desktop GMA950"	},
+	{ 0x80862776, "Desktop GMA950"	},
+//	{ 0x8086A001, "Desktop GMA3150" },
+	{ 0x8086A001, "Mobile GMA3150"	},
+	{ 0x8086A002, "Desktop GMA3150" },
+	{ 0x80862A02, "GMAX3100"		},
+	{ 0x80862A03, "GMAX3100"		},
+	{ 0x80862A12, "GMAX3100"		},
+	{ 0x80862A13, "GMAX3100"		},
+	{ 0x80862A42, "GMAX3100"		},
+	{ 0x80862A43, "GMAX3100"		},
+	{ 0x80860102, "Intel HD Graphics 2000"			},
+	{ 0x80860106, "Intel HD Graphics 2000 Mobile"	},
+	{ 0x80860112, "Intel HD Graphics 3000"			},
+	{ 0x80860116, "Intel HD Graphics 3000 Mobile"	},
+	{ 0x80860122, "Intel HD Graphics 3000"			},
+	{ 0x80860126, "Intel HD Graphics 3000 Mobile"	},
+};
+
+char *get_gma_model(uint32_t id) {
+	int i = 0;
+	
+	for (i = 0; i < (sizeof(KnownGPUS) / sizeof(KnownGPUS[0])); i++)
+	{
+		if (KnownGPUS[i].device == id)
+			return KnownGPUS[i].name;
+	}
+	return KnownGPUS[0].name;
+}
+
+bool setup_gma_devprop(pci_dt_t *gma_dev)
+{
+	char					*devicepath;
+	volatile uint8_t		*regs;
+	uint32_t				bar[7];
+	char					*model;
+	uint8_t BuiltIn =		0x00;
+	uint8_t ClassFix[4] =	{ 0x00, 0x00, 0x03, 0x00 };
+	unsigned int			device_id;	
+	
+	devicepath = get_pci_dev_path(gma_dev);
+	
+	bar[0] = pci_config_read32(gma_dev->dev.addr, 0x10);
+	regs = (uint8_t *) (bar[0] & ~0x0f);
+	
+	model = get_gma_model((gma_dev->vendor_id << 16) | gma_dev->device_id);
+	device_id = gma_dev->device_id;
+	
+	verbose("Intel %s [%04x:%04x] :: %s\n",
+			model, gma_dev->vendor_id, gma_dev->device_id, devicepath);
+	
+	if (!string)
+		string = devprop_create_string();
+	
+	struct DevPropDevice *device = malloc(sizeof(struct DevPropDevice));
+	device = devprop_add_device(string, devicepath);
+	
+	if (!device)
+	{
+		printf("Failed initializing dev-prop string dev-entry.\n");
+		pause();
+		return false;
+	}
+	
+	devprop_add_value(device, "model", (uint8_t*)model, (strlen(model) + 1));
+	devprop_add_value(device, "device_type", (uint8_t*)"display", 8);	
+	
+	if ((model == (char *)"Mobile GMA950")
+		|| (model == (char *)"Mobile GMA3150"))
+	{
+		devprop_add_value(device, "AAPL,HasPanel", reg_TRUE, 4);
+		devprop_add_value(device, "built-in", &BuiltIn, 1);
+		devprop_add_value(device, "class-code", ClassFix, 4);
+	}
+	else if ((model == (char *)"Desktop GMA950")
+			|| (model == (char *)"Desktop GMA3150"))
+	{
+		BuiltIn = 0x01;
+		devprop_add_value(device, "built-in", &BuiltIn, 1);
+		devprop_add_value(device, "class-code", ClassFix, 4);
+	}
+	else if (model == (char *)"GMAX3100")
+	{
+		devprop_add_value(device, "AAPL,HasPanel",					GMAX3100_vals[0], 4);
+		devprop_add_value(device, "AAPL,SelfRefreshSupported",		GMAX3100_vals[1], 4);
+		devprop_add_value(device, "AAPL,aux-power-connected",		GMAX3100_vals[2], 4);
+		devprop_add_value(device, "AAPL,backlight-control",			GMAX3100_vals[3], 4);
+		devprop_add_value(device, "AAPL00,blackscreen-preferences", GMAX3100_vals[4], 4);
+		devprop_add_value(device, "AAPL01,BacklightIntensity",		GMAX3100_vals[5], 4);
+		devprop_add_value(device, "AAPL01,blackscreen-preferences", GMAX3100_vals[6], 4);
+		devprop_add_value(device, "AAPL01,DataJustify",				GMAX3100_vals[7], 4);
+		devprop_add_value(device, "AAPL01,Depth",					GMAX3100_vals[8], 4);
+		devprop_add_value(device, "AAPL01,Dither",					GMAX3100_vals[9], 4);
+		devprop_add_value(device, "AAPL01,DualLink",				GMAX3100_vals[10], 4);
+		devprop_add_value(device, "AAPL01,Height",					GMAX3100_vals[11], 4);
+		devprop_add_value(device, "AAPL01,Interlace",				GMAX3100_vals[12], 4);
+		devprop_add_value(device, "AAPL01,Inverter",				GMAX3100_vals[13], 4);
+		devprop_add_value(device, "AAPL01,InverterCurrent",			GMAX3100_vals[14], 4);
+		devprop_add_value(device, "AAPL01,InverterCurrency",		GMAX3100_vals[15], 4);
+		devprop_add_value(device, "AAPL01,LinkFormat",				GMAX3100_vals[16], 4);
+		devprop_add_value(device, "AAPL01,LinkType",				GMAX3100_vals[17], 4);
+		devprop_add_value(device, "AAPL01,Pipe",					GMAX3100_vals[18], 4);
+		devprop_add_value(device, "AAPL01,PixelFormat",				GMAX3100_vals[19], 4);
+		devprop_add_value(device, "AAPL01,Refresh",					GMAX3100_vals[20], 4);
+		devprop_add_value(device, "AAPL01,Stretch",					GMAX3100_vals[21], 4);
+		devprop_add_value(device, "class-code",						ClassFix, 4);
+	}
+	else if (model == (char *)"Intel HD Graphics 2000 Mobile")
+	{
+		devprop_add_value(device, "class-code", ClassFix, 4);
+		devprop_add_value(device, "hda-gfx", (uint8_t *)"onboard-1", 10); 
+		devprop_add_value(device, "AAPL00,PixelFormat", HD2000_vals[0], 4);
+		devprop_add_value(device, "AAPL00,T1", HD2000_vals[1], 4);
+		devprop_add_value(device, "AAPL00,T2", HD2000_vals[2], 4);
+		devprop_add_value(device, "AAPL00,T3", HD2000_vals[3], 4);
+		devprop_add_value(device, "AAPL00,T4", HD2000_vals[4], 4);
+		devprop_add_value(device, "AAPL00,T5", HD2000_vals[5], 4);
+		devprop_add_value(device, "AAPL00,T6", HD2000_vals[6], 4);
+		devprop_add_value(device, "AAPL00,T7", HD2000_vals[7], 4);
+		devprop_add_value(device, "AAPL00,LinkType", HD2000_vals[8], 4);
+		devprop_add_value(device, "AAPL00,LinkFormat", HD2000_vals[9], 4);
+		devprop_add_value(device, "AAPL00,DualLink", HD2000_vals[10], 4);
+		devprop_add_value(device, "AAPL00,Dither", HD2000_vals[11], 4);
+		devprop_add_value(device, "AAPL00,DataJustify", HD3000_vals[12], 4);
+		devprop_add_value(device, "graphic-options", HD2000_vals[13], 4);
+		devprop_add_value(device, "AAPL,tbl-info", HD2000_tbl_info, 18);
+		devprop_add_value(device, "AAPL,os-info", HD2000_os_info, 20);
+	}
+	else if (model == (char *)"Intel HD Graphics 3000 Mobile")
+	{
+		devprop_add_value(device, "class-code", ClassFix, 4);
+		devprop_add_value(device, "hda-gfx", (uint8_t *)"onboard-1", 10); 
+		devprop_add_value(device, "AAPL00,PixelFormat", HD3000_vals[0], 4);
+		devprop_add_value(device, "AAPL00,T1", HD3000_vals[1], 4);
+		devprop_add_value(device, "AAPL00,T2", HD3000_vals[2], 4);
+		devprop_add_value(device, "AAPL00,T3", HD3000_vals[3], 4);
+		devprop_add_value(device, "AAPL00,T4", HD3000_vals[4], 4);
+		devprop_add_value(device, "AAPL00,T5", HD3000_vals[5], 4);
+		devprop_add_value(device, "AAPL00,T6", HD3000_vals[6], 4);
+		devprop_add_value(device, "AAPL00,T7", HD3000_vals[7], 4);
+		devprop_add_value(device, "AAPL00,LinkType", HD3000_vals[8], 4);
+		devprop_add_value(device, "AAPL00,LinkFormat", HD3000_vals[9], 4);
+		devprop_add_value(device, "AAPL00,DualLink", HD3000_vals[10], 4);
+		devprop_add_value(device, "AAPL00,Dither", HD3000_vals[11], 4);
+		devprop_add_value(device, "AAPL00,DataJustify", HD3000_vals[12], 4);
+		devprop_add_value(device, "graphic-options", HD3000_vals[13], 4);
+		devprop_add_value(device, "AAPL,tbl-info", HD3000_tbl_info, 18);
+		devprop_add_value(device, "AAPL,os-info", HD3000_os_info, 20);
+	}
+	else if (model == (char *)"Intel HD Graphics 2000")
+	{
+		devprop_add_value(device, "built-in", &BuiltIn, 1);
+		devprop_add_value(device, "class-code", ClassFix, 4);
+		devprop_add_value(device, "device-id", (uint8_t*)&device_id, sizeof(device_id));
+		devprop_add_value(device, "hda-gfx", (uint8_t *)"onboard-1", 10); 
+		devprop_add_value(device, "AAPL,tbl-info", HD2000_tbl_info, 18);
+		devprop_add_value(device, "AAPL,os-info", HD2000_os_info, 20);
+	}
+	else if (model == (char *)"Intel HD Graphics 3000")
+	{
+		devprop_add_value(device, "built-in", &BuiltIn, 1);
+		devprop_add_value(device, "class-code", ClassFix, 4);
+		device_id = 0x00000126;											// Inject a valid mobile GPU device id instead of patching kexts
+		devprop_add_value(device, "device-id", (uint8_t*)&device_id, sizeof(device_id));
+		devprop_add_value(device, "hda-gfx", (uint8_t *)"onboard-1", 10); 
+		devprop_add_value(device, "AAPL,tbl-info", HD3000_tbl_info, 18);
+		devprop_add_value(device, "AAPL,os-info", HD3000_os_info, 20);
+	}
+	
+	stringdata = malloc(sizeof(uint8_t) * string->length);
+	if (!stringdata)
+	{
+		printf("No stringdata.\n");
+		pause();
+		return false;
+	}
+	
+	memcpy(stringdata, (uint8_t*)devprop_generate_string(string), string->length);
+	stringlength = string->length;
+	
+	return true;
+}
---
+++ Trunk/package/Scripts/Sub/CheckProceed.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+echo "==============================================="
+echo "Check Proceed: Can the installation continue?"
+echo "***********************************************"
+
+# Checks the selected volume is present and the disk is partitioned
+# Now also check for another existing Chameleon installation on the same disk.
+
+# Receives targetDisk: for example, /dev/disk3.
+# Receives targetDeviceRaw: for example, /dev/rdisk3s1.
+# Receives targetVolume: Volume to install to (will be '/Volumes/EFI' if EFI install)
+# Receives targetDevice: Stores device number, for example /dev/disk2s1.
+# Receives installerVolume: Volume to write the installer log to.
+# Receives scriptDir: The location of the main script dir.
+
+if [ "$#" -eq 6 ]; then
+	targetDisk="$1"
+	targetDeviceRaw="$2"
+	targetVolume="$3"
+	targetDevice="$4"
+	installerVolume="$5"
+	scriptDir="$6"
+	echo "DEBUG: passed argument for targetDisk = $targetDisk"
+	echo "DEBUG: passed argument for targetDeviceRaw = $targetDeviceRaw"
+	echo "DEBUG: passed argument for targetVolume = $targetVolume"
+	echo "DEBUG: passed argument for targetDevice = $targetDevice"
+	echo "DEBUG: passed argument for installerVolume = $installerVolume"
+	echo "DEBUG: passed argument for scriptDir = $scriptDir"
+else
+	echo "Error - wrong number of values passed"
+	exit 9
+fi
+
+
+# Does target volume exist?
+if [ -z "$targetVolume" ]; then
+	echo "*** Cannot find the volume. Exiting."
+	"$scriptDir"InstallLog.sh "${installerVolume}" "FAIL: Cannot file the volume: $targetVolume."
+	exit 1
+#else
+	#echo "DEBUG: Confirming target volume exists"
+fi
+
+
+# Does target volume use slices?
+if [ "$targetDevice" = "$targetDevice#*disk*s" ]; then
+	echo "*** ERROR Volume does not use slices. Exiting."
+	"$scriptDir"InstallLog.sh "${installerVolume}" "FAIL: $targetVolume doesn't use slices."
+	exit 1		
+#else
+	#echo "DEBUG: Confirming target device uses slices"
+fi
+
+
+# Add check for installing to a 'small' HFS device like a
+# 1GB USB flash drive which won't have an EFI System Partition.
+if [ "$targetVolume" = "/Volumes/EFI" ]; then
+	# Take target device and check slice 1 matches partition named "EFI"
+	stripped=$( echo ${targetDevice#/dev/} )
+	if [ ! $(echo ${stripped#*disk*s}) = 1 ]; then
+		stripped=$( echo ${stripped%s*})"s1"
+	fi
+	if [ ! $( diskutil list | grep ${stripped} | awk {'print $2'} ) = "EFI" ]; then
+		#echo "DEBUG: *** The selected volume doesn't have an EFI System Partition. Exiting."
+		"$scriptDir"InstallLog.sh "${installerVolume}" "FAIL: Selected disk does not have an EFI System Partition."
+		exit 1
+	fi
+fi
+
+
+# Check for existing Chameleon installations on a different
+# partition of the same target disk.
+#echo "DEBUG: Checking for existing Chameleon installations on ${targetDisk#/dev/}..."
+
+sliceNumber=$( echo ${targetDeviceRaw#*disk*s} )
+# strip slice from end
+targetDiskRawNoSlice=$( echo ${targetDeviceRaw%$sliceNumber} )
+
+# Are there any other partitions on the disk?
+# How many actual partitions are there?
+numSlices=$(( $( diskutil list | grep $( echo ${targetDisk#/dev/} ) | sed -n '$=' ) -2 ))
+
+# Only check the disk for Chameleon installations if there is more than one partition.
+if [ $numSlices -gt 1 ]; then 
+		
+	#Scan all partitions for Chameleon code
+	for (( i=1; i <= $numSlices; i++ ));
+	do
+		stageExistence=0
+		targetDiskRaw=$targetDiskRawNoSlice$i
+
+		# Check for existing stage 0 boot file from CheckDiskMicrocode.sh script
+		stage0type=$( dd 2>/dev/null if="$targetDisk" count=3 bs=1 skip=105 | xxd | awk '{print $2$3}' )
+		if [ "${stage0type}" == "0b807c" ] || [ "${stage0type}" == "0a803c" ] || [ "${stage0type}" == "ee7505" ]; then
+			#echo "DEBUG: boot0 found on $targetDisk"
+			(( stageExistence++ ))
+		fi
+			
+		# Check for boot1h and boot1f32
+		boot1hSearch=$( dd 2>/dev/null if="$targetDiskRaw" count=1 | perl -ne '@a=split"";for(@a){printf"%02x",ord}' )
+		if [ "${boot1hSearch:0:16}" == "fa31c08ed0bcf0ff" ]; then
+			#echo "DEBUG: boot1h found on "$targetDiskRaw
+			(( stageExistence++ ))
+		elif [ "${boot1hSearch:0:4}" == "e962" ] && [ "${boot1hSearch:180:12}" == "424f4f542020" ]; then
+			#echo "DEBUG: boot1f32 found on "$targetDiskRaw
+			(( stageExistence++ ))
+		fi
+		
+		# Check for existing stage 2 boot file also
+		if [ -e "$( df | grep $targetDisk"s"$i | awk '{ print $6 }' )"/boot ]; then
+			#echo "DEBUG: boot found on $targetDiskRaw"
+			(( stageExistence++ ))
+		fi
+		
+		if [ $stageExistence == 3 ] && [ $i -ne $sliceNumber ]; then
+			#echo "DEBUG: STOP: There is already an existing Chameleon installation on $targetDiskRaw"
+			"$scriptDir"InstallLog.sh "${installerVolume}" "STOP: There is already an existing Chameleon installation on $targetDiskRaw."
+			"$scriptDir"InstallLog.sh "${installerVolume}" "NOTE: This is allowed and does work as long as you aren't dual booting Windows"
+			"$scriptDir"InstallLog.sh "${installerVolume}" "NOTE: from the same disk and are happy to control which partition is used by"
+			"$scriptDir"InstallLog.sh "${installerVolume}" "NOTE: flagging the required partition active. General use doesn't require two"
+			"$scriptDir"InstallLog.sh "${installerVolume}" "NOTE: Chameleon installs on the same disk, though might be done by advanced users."
+			"$scriptDir"InstallLog.sh "${installerVolume}" "NOTE: If you still want to do this then proceed by installing Chameleon manually."
+			exit 1
+		fi
+	done
+
+#else
+	#echo "DEBUG: Just one slice"
+fi
+
+#"$scriptDir"InstallLog.sh "${installerVolume}" "CheckProceed: PASS"
+
+exit 0
