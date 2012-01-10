a1e3f567ed2c2a35118d2e6fa35f515f9a6e47e5
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2012-01-10 17:52:59 +0000

    Merge last main trunk change. - Update Welcome.rtfd/TXT.rtf for each language.

--- Modules/package/buildpkg.sh
+++ Modules/package/buildpkg.sh
@@ -170,9 +170,11 @@ $chameleonSubsts
 $ownSubst"
 
     for file in "$@";do
-        cp -pf "$file" "${file}.in"
-        sed "$allSubst" "${file}.in" > "${file}"
-        rm -f "${file}.in"
+        if [[ ! $file == *.svn* ]]; then
+            cp -pf "$file" "${file}.in"
+            sed "$allSubst" "${file}.in" > "${file}"
+            rm -f "${file}.in"
+        fi
     done
 }
 
