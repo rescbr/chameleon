c4afe4d616fbfba4fd28275643b306606e6545ec
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2012-03-03 23:37:02 +0000

    Merge latest JrCs's changes and setup for translation: - Move old Resources and create bin/po4a - Cleanup and Setup for translation - Add PO files - Add po4a configuration

--- Trunk/package/buildpkg.sh
+++ Trunk/package/buildpkg.sh
@@ -986,17 +986,27 @@ makedistribution ()
     echo -e "\n</installer-gui-script>"  >> "${PKG_BUILD_DIR}/${packagename}/Distribution"
 
 #   Create the Resources directory
-    ditto --noextattr --noqtn "${PKGROOT}/Resources" "${PKG_BUILD_DIR}/${packagename}/Resources"
+    ditto --noextattr --noqtn "${PKGROOT}/Resources/distribution" "${PKG_BUILD_DIR}/${packagename}/Resources"
 
-#   CleanUp the directory
-    # this next line should work but it doesn't - not sure why.
-    #find "${PKG_BUILD_DIR}/${packagename}" \( -type d -name '.svn' \) -o -name '.DS_Store' -exec rm -rf {} \;
+#   Make the translation
+    echo ""
+    echo "========= Translating Resources ========"
+    (cd "${PKGROOT}" &&  PERLLIB=${PKGROOT}/bin/po4a/lib          \
+        bin/po4a/po4a                                             \
+        --msgid-bugs-address po4a-devel@lists.alioth.debian.org   \
+        --package-name chameleon                                  \
+        --package-version 1.2.3                                   \
+        --variable PODIR="po"                                     \
+        --variable TEMPLATES_DIR="Resources/templates"            \
+        --variable OUTPUT_DIR="${PKG_BUILD_DIR}/${packagename}/Resources" \
+        ${PKGROOT}/po4a-chameleon.cfg)
     
-    # instead, doing it this way works.
-    find "${PKG_BUILD_DIR}/${packagename}/Resources" -name ".svn" -type d -o -name ".DS_Store" -type f | while read component
-    do
-		rm -rf "${component}"
-    done    
+    # Copy common files in english localisation directory
+    ditto --noextattr --noqtn "${PKGROOT}/Resources/common" "${PKG_BUILD_DIR}/${packagename}/Resources/en.lproj"
+    
+    # CleanUp the directory
+    find "${PKG_BUILD_DIR}/${packagename}" \( -type d -name '.svn' \) -o -name '.DS_Store' -exec rm -rf {} \;
+    find "${PKG_BUILD_DIR}/${packagename}" -type d -depth -empty -exec rmdir {} \; # Remove empty directories
 
     # Make substitutions for version, revision, stage, developers, credits, etc..
     makeSubstitutions $( find "${PKG_BUILD_DIR}/${packagename}/Resources" -type f )
