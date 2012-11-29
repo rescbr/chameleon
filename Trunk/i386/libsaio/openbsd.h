f45bea2881e345070d5723b941423746de4c37b3
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2012-11-29 16:26:35 +0000

    Restructuring my branch work in progress...

--- Trunk/i386/libsaio/openbsd.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LIBSAIO_OPENBSD_H
-#define __LIBSAIO_OPENBSD_H
-
-extern bool OpenBSDProbe (const void *buf);
-extern void OpenBSDGetDescription(CICell ih, char *str, long strMaxLen);
-
-#endif /* !__LIBSAIO_OPENBSD_H */
--- Trunk/i386/modules/Keylayout/Readme.txt
+++
@@ -1,3 +0,0 @@
-Module:	Keylayout
-Description: This module permits the user to define the keyboard key mappings
-Dependencies: none
