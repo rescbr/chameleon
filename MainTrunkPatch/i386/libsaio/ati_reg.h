ddcdccdc174b933fc710531ddc448f248682fb95
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2012-12-16 14:19:36 +0000

    Update Ati, add support for 7000 Series (From Enoch, by ErmaC).

--- MainTrunkPatch/i386/libsaio/ati_reg.h
+++ MainTrunkPatch/i386/libsaio/ati_reg.h
@@ -52,6 +52,223 @@
 #ifndef _ATI_REG_H_
 #define _ATI_REG_H_
 
+
+#define OFFSET_TO_GET_ATOMBIOS_STRINGS_START 0x6e
+#define DATVAL(x)			{kPtr, sizeof(x), (uint8_t *)x}
+#define STRVAL(x)			{kStr, sizeof(x)-1, (uint8_t *)x}
+#define BYTVAL(x)			{kCst, 1, (uint8_t *)x}
+#define WRDVAL(x)			{kCst, 2, (uint8_t *)x}
+#define DWRVAL(x)			{kCst, 4, (uint8_t *)x}
+//#define QWRVAL(x)			{kCst, 8, (uint8_t *)x}
+#define NULVAL				{kNul, 0, (uint8_t *)NULL}
+
+typedef enum {
+	kNul,
+	kStr,
+	kPtr,
+	kCst
+} type_t;
+
+typedef enum {
+	CHIP_FAMILY_UNKNOW,
+	/* Old */
+	CHIP_FAMILY_R420,
+	CHIP_FAMILY_RV410,
+	CHIP_FAMILY_RV515,
+	CHIP_FAMILY_R520,
+	CHIP_FAMILY_RV530,
+	CHIP_FAMILY_RV560,
+	CHIP_FAMILY_RV570,
+	CHIP_FAMILY_R580,
+	/* IGP */
+	CHIP_FAMILY_RS600,
+	CHIP_FAMILY_RS690,
+	CHIP_FAMILY_RS740,
+	CHIP_FAMILY_RS780,
+	CHIP_FAMILY_RS880,
+	/* R600 */
+	CHIP_FAMILY_R600,
+	CHIP_FAMILY_RV610,
+	CHIP_FAMILY_RV620,
+	CHIP_FAMILY_RV630,
+	CHIP_FAMILY_RV635,
+	CHIP_FAMILY_RV670,
+	/* R700 */
+	CHIP_FAMILY_RV710,
+	CHIP_FAMILY_RV730,
+	CHIP_FAMILY_RV740,
+	CHIP_FAMILY_RV770,
+	CHIP_FAMILY_RV772,
+	CHIP_FAMILY_RV790,
+	/* Evergreen */
+	CHIP_FAMILY_CEDAR,
+	CHIP_FAMILY_CYPRESS,
+	CHIP_FAMILY_HEMLOCK,
+	CHIP_FAMILY_JUNIPER,
+	CHIP_FAMILY_REDWOOD,
+	CHIP_FAMILY_BROADWAY,
+    //	CHIP_FAMILY_MADISON,
+    //	CHIP_FAMILY_PARK,
+	/* Northern Islands */
+    //	CHIP_FAMILY_ANTILLES,
+	CHIP_FAMILY_BARTS,
+	CHIP_FAMILY_CAICOS,
+	CHIP_FAMILY_CAYMAN,
+	CHIP_FAMILY_TURKS,
+	/* Southern Islands */
+	CHIP_FAMILY_TAHITI,
+	CHIP_FAMILY_PITCAIRN,
+    //	CHIP_FAMILY_VERDE,
+    //	CHIP_FAMILY_THAMES,
+    CHIP_FAMILY_LOMBOK,
+    //	CHIP_FAMILY_NEWZEALAND,
+	CHIP_FAMILY_LAST
+} ati_chip_family_t;
+
+typedef struct {
+	const char		*name;
+	uint8_t			ports;
+} card_config_t;
+
+typedef enum {
+	kNull,
+/* OLDController */
+	kWormy,
+	kAlopias,
+	kCaretta,
+	kKakapo,
+	kKipunji,
+	kPeregrine,
+	kRaven,
+	kSphyrna,
+/* AMD2400Controller */
+	kIago,
+/* AMD2600Controller */
+	kHypoprion,
+	kLamna,
+/* AMD3800Controller */
+	kMegalodon,
+	kTriakis,
+/* AMD4600Controller */
+	kFlicker,
+	kGliff,
+	kShrike,
+/* AMD4800Controller */
+	kCardinal,
+	kMotmot,
+	kQuail,
+/* AMD5000Controller */
+	kDouc,
+	kLangur,
+	kUakari,
+	kZonalis,
+	kAlouatta,
+	kHoolock,
+	kVervet,
+	kBaboon,
+	kEulemur,
+	kGalago,
+	kColobus,
+	kMangabey,
+	kNomascus,
+	kOrangutan,
+/* AMD6000Controller */
+	kPithecia,
+	kBulrushes,
+	kCattail,
+	kHydrilla,
+	kDuckweed,
+	kFanwort,
+	kElodea,
+	kKudzu,
+	kGibba,
+	kLotus,
+	kIpomoea,
+	kMuskgrass,
+	kJuncus,
+	kOsmunda,
+	kPondweed,
+	kSpikerush,
+	kTypha,
+/* AMD7000Controller */
+	kAji, // TESTING
+	kBuri, // TESTING
+	kChutoro, // TESTING
+	kDashimaki, // TESTING
+	kEbi, // TESTING
+	kGari, // TESTING
+	kFutomaki, // TESTING
+	kHamachi, // TESTING
+	kOPM, // TESTING
+	kIkura, // TESTING
+	kIkuraS, // TESTING
+	kCfgEnd
+} config_name_t;
+
+typedef struct {
+	uint16_t			device_id;
+	uint32_t			subsys_id;
+	ati_chip_family_t		chip_family;
+	const char			*model_name;
+	config_name_t			cfg_name;
+} radeon_card_info_t;
+
+typedef struct {
+	struct DevPropDevice		*device;
+	radeon_card_info_t		*info;
+	pci_dt_t			*pci_dev;
+	uint8_t				*fb;
+	uint8_t				*mmio;
+	uint8_t				*io;
+	uint8_t				*rom;
+	uint32_t			rom_size;
+	uint32_t			vram_size;
+	const char			*cfg_name;
+	uint8_t				ports;
+	uint32_t			flags;
+	bool				posted;
+} card_t;
+card_t *card;
+
+/* Flags */
+#define MKFLAG(n)			(1 << n)
+#define FLAGTRUE			MKFLAG(0)
+#define EVERGREEN			MKFLAG(1)
+#define FLAGMOBILE			MKFLAG(2)
+
+//static uint8_t atN = 0;
+
+typedef struct {
+	type_t				type;
+	uint32_t			size;
+	uint8_t				*data;
+} value_t;
+
+typedef struct {
+	uint32_t			flags;
+	bool				all_ports;
+	char				*name;
+	bool				(*get_value)(value_t *val);
+	value_t				default_val;
+} dev_prop_t;
+
+bool get_bootdisplay_val(value_t *val);
+bool get_vrammemory_val(value_t *val);
+bool get_name_val(value_t *val);
+bool get_nameparent_val(value_t *val);
+bool get_model_val(value_t *val);
+bool get_conntype_val(value_t *val);
+bool get_vrammemsize_val(value_t *val);
+bool get_binimage_val(value_t *val);
+bool get_romrevision_val(value_t *val);
+bool get_deviceid_val(value_t *val);
+bool get_mclk_val(value_t *val);
+bool get_sclk_val(value_t *val);
+bool get_refclk_val(value_t *val);
+bool get_platforminfo_val(value_t *val);
+bool get_vramtotalsize_val(value_t *val);
+bool get_hdmiaudio(value_t * val);
+
 #define ATI_DATATYPE_VQ				0
 #define ATI_DATATYPE_CI4			1
 #define ATI_DATATYPE_CI8			2
