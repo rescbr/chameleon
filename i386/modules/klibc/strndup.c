6eac194b07ac177c02b53f5543bf486b20c13975
bitshoveler <bitshoveler@b5af796a-00a8-11df-919b-ffff7a100b7d>
2014-10-23 03:51:20 +0000

    Only write terminating null char if malloc succeeded

--- i386/modules/klibc/strndup.c
+++ i386/modules/klibc/strndup.c
@@ -9,8 +9,9 @@ char *strndup(const char *s, size_t n)
 	int l = n > strlen(s) ? strlen(s) + 1 : n + 1;
 	char *d = malloc(l);
 
-	if (d)
-		memcpy(d, s, l);
-	d[n] = '\0';
+	if (d) {
+      memcpy(d, s, l);
+      d[n] = '\0';
+    }
 	return d;
 }
