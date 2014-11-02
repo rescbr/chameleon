e9162b5de2683a39913d21a55fa7c7c6e742ad07
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2014-11-02 00:37:58 +0000

    Split out states generator from acpi_patcher (Credits to Clover Teams)

---
+++ Enoch/i386/libsaio/state_generator.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright 2008 mackerintel
+ *
+ *  state_generator.h
+ *  Chameleon
+ *
+ *  Created by Mozodojo on 20/07/10.
+ *  Copyright 2010 mozo. All rights reserved.
+ *
+ */
+
+#ifndef __LIBSAIO_STATE_GENERATOR_H
+#define __LIBSAIO_STATE_GENERATOR_H
+
+#include "aml_generator.h"
+#include "libsaio.h"
+
+void get_acpi_cpu_names(uint8_t* dsdt, uint32_t length);
+struct acpi_2_ssdt *generate_cst_ssdt(struct acpi_2_fadt* fadt);
+struct acpi_2_ssdt *generate_pss_ssdt(struct acpi_2_dsdt* dsdt);
+
+#endif /* !__LIBSAIO_STATE_GENERATOR_H */
