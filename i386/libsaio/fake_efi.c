3345d0c5cd1a8562a0d4a2334c87002d836db037
ifabio <ifabio@b5af796a-00a8-11df-919b-ffff7a100b7d>
2015-03-15 09:46:45 +0000

    Move and rename GUID Tables into efi header.

--- i386/libsaio/fake_efi.c
+++ i386/libsaio/fake_efi.c
@@ -421,7 +421,7 @@ static const char CPU_Frequency_prop[] = "CPUFrequency";
  */
 
 /* From Foundation/Efi/Guid/Smbios/SmBios.c */
-EFI_GUID const	gEfiSmbiosTableGuid = EFI_SMBIOS_TABLE_GUID;
+EFI_GUID const	gEfiSmbiosTableGuid = SMBIOS_TABLE_GUID;
 
 #define SMBIOS_RANGE_START		0x000F0000
 #define SMBIOS_RANGE_END		0x000FFFFF
@@ -429,18 +429,8 @@ EFI_GUID const	gEfiSmbiosTableGuid = EFI_SMBIOS_TABLE_GUID;
 /* '_SM_' in little endian: */
 #define SMBIOS_ANCHOR_UINT32_LE 0x5f4d535f
 
-#define EFI_ACPI_TABLE_GUID \
-{ \
-	0xeb9d2d30, 0x2d88, 0x11d3, { 0x9a, 0x16, 0x0, 0x90, 0x27, 0x3f, 0xc1, 0x4d } \
-}
-
-#define EFI_ACPI_20_TABLE_GUID \
-{ \
-	0x8868e871, 0xe4f1, 0x11d3, { 0xbc, 0x22, 0x0, 0x80, 0xc7, 0x3c, 0x88, 0x81 } \
-}
-
-EFI_GUID gEfiAcpiTableGuid = EFI_ACPI_TABLE_GUID;
-EFI_GUID gEfiAcpi20TableGuid = EFI_ACPI_20_TABLE_GUID;
+EFI_GUID gEfiAcpiTableGuid = ACPI_TABLE_GUID;
+EFI_GUID gEfiAcpi20TableGuid = ACPI_20_TABLE_GUID;
 
 
 /*==========================================================================
